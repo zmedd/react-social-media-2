620bcdce9324ea7a2a27ed58bed1385bde59e1b2
Zmed Alex <zmed.alex@gmail.com>
2021-07-27 19:29:39 +0300

    add history to router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Login, Register, Newsfeed } from "./pages";
+import { useHistory } from "react-router-dom";
 
 function App() {
+  const history = useHistory();
+
   return (
-    <Router>
+    <Router history={history}>
       <Switch>
         <Route path="/" exact component={Newsfeed} />
         <Route path="/login" component={Login} />
